4f74cbfde2559d3ce3e4c86cb06a1476deddf22d
ammad_nazir <mughal.amd@gmail.com>
2015-03-10 15:44:50 +0500

    Some Update
    Not So Much

--- controllers/admin.php
+++ controllers/admin.php
@@ -7,10 +7,30 @@
 class Admin extends Controller 
 {
     public function __construct(){parent::__construct();}
-
+    
+    // just a view of site
+    // main Index Function
+    // admin index front land page after login 
+    // security not active yet
     public function index()
     {
+        // Title
+        $this->page->title = "Welcome Note &mdash; ".SITE_NAME;
+        // Main header.php in view/admin/header.php 
+        $this->page->view('admin/header');
+        // Main index.php in view/admin/index.php 
         $this->page->view('admin/index');
+    }
+    
+    // media page just view
+    public function media()
+    {
+        // Title
+        $this->page->title = "Media View &mdash; ".SITE_NAME;
+        // Main header.php in view/admin/header.php 
+        $this->page->view('admin/header');
+        // Main index.php in view/admin/index.php 
+        $this->page->view('admin/media');
     }
 
 }
--- views/admin/index.php
+++ views/admin/index.php
@@ -1,46 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Simple CMS</title>
-
-        <!-- CSS -->
-        <link href="assets/admin/css/bootstrap.min.css" rel="stylesheet"/>
-        <link href="assets/admin/css/layout.css" rel="stylesheet"/>
-    </head>
-    <body>
-            <div class="mcadminbar navbar-fixed-top">
-                <div class="mclogo pull-left">
-                    <a href="#" title="Mato CMS"><span class="glyphicon glyphicon-globe"></span></a>
-                </div><!-- /.maker-logo -->
-                <!--        <div class="mc-visitsite">
-                                <a href="#"><span class="glyphicon glyphicon-new-window"></span></a>
-                            </div>-->
-                <div class="mcadmindetail pull-right">
-                    <div class="mcadminimg pull-left">
-                        <!--<img src="" class="glyphicon glyphicon-user">-->
-    <!--                    <span class="glyphicon glyphicon-user"></span>-->
-                    </div>
-                    <div class="msadmindropdown">
-                        <span class="glyphicon glyphicon-menu-down"></span>
-                    </div>
-                </div>
-            
-            <div class="mcadminnavigation">
-                <ul>
-                    <li><a href="">Dashboard</a></li>
-                    <li><a href="">Media</a></li>
-                    <li><a href="">Pages</a></li>
-                    <li><a href="">Posts</a></li>
-                    <li><a href="">Comments</a></li>
-                    <li><a href="">Web Design</a></li>
-                    <li><a href="">Users</a></li>
-                </ul>
-            </div><!-- /.mc-navigation -->
-            </div><!-- /.header -->
             <div class="mcfrontsitedetail">
                 <div class="wrap">
                     <h2>Dashboard</h2>
